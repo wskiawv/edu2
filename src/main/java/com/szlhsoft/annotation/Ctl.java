43b0e0734201bc0dcade2ced7c39cbb9bd3bae03
wskiawv <wskiawv@163.com>
2018-09-19 22:37:58 +0800

    添加分支

---
+++ src/main/java/com/szlhsoft/annotation/Ctl.java
@@ -0,0 +1,15 @@
+package com.szlhsoft.core.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface Ctl {
+	String name() default "";
+	String module() default "";
+	String code() default "";
+}
---
+++ src/main/java/com/szlhsoft/controller/base/BaseController.java
@@ -0,0 +1,7 @@
+package com.szlhsoft.controller.base;
+
+/**
+ * Created by Administrator on 2018/9/19.
+ */
+public class BaseController {
+}
