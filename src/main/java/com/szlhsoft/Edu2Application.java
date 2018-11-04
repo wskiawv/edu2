0104fd65dcdec5418a1911b0e1e4b744d57e3034
wskiawv <wskiawv@163.com>
2018-11-04 22:34:28 +0800

    测试Log

--- src/main/java/com/szlhsoft/Edu2Application.java
+++ src/main/java/com/szlhsoft/Edu2Application.java
@@ -1,15 +1,16 @@
 package com.szlhsoft;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Edu2Application {
-
+@EnableTransactionManagement
+public class Edu2Application   {
 	public static void main(String[] args) {
 		SpringApplication.run(Edu2Application.class, args);
 	}
